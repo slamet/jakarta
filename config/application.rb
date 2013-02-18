2f89c552489f49d58d3c6f900f9ec03b4cac7310
slamet <slametnhd@gmail.com>
2013-02-17 22:10:33 -0800

    resume

--- config/application.rb
+++ config/application.rb
@@ -54,9 +54,11 @@ module Aplikasi
     config.assets.enabled = true
 
 
-    config.assets.initialize_on_precompile = false
+
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
